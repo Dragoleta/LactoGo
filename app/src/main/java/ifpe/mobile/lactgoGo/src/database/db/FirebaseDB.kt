88b7905b9be00364cf2799942adee763ce0049c8
JorgeD <jorgelucasdantas@hotmail.com>
2024-09-11 13:23:58 -0300

    Starting database and ui integration

--- app/src/main/java/ifpe/mobile/lactgoGo/src/database/db/FirebaseDB.kt
+++ app/src/main/java/ifpe/mobile/lactgoGo/src/database/db/FirebaseDB.kt
@@ -1,46 +1,70 @@
 package ifpe.mobile.lactgoGo.src.database.db
 
+import FBUser
 import User
+import android.util.Log
+import androidx.compose.runtime.Composable
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
+import ifpe.mobile.lactgoGo.src.database.models.Restaurant
 import toFBUser
+import toUser
 
 class FirebaseDB(private val listener: Listener? = null) {
     private val auth = Firebase.auth
     private val db = Firebase.firestore
     interface Listener {
+        fun onUserLoaded(user: User)
+        fun setRestaurants(rests: List<Restaurant>)
+//        fun onCityAdded(city: City)
+//        fun onCityRemoved(city: City)
     }
 
-////    private var citiesListReg: ListenerRegistration? = null
-//    interface Listener {
-////        fun onUserLoaded(user: User)
-////        fun onCityAdded(city: City)
-////        fun onCityRemoved(city: City)
-//    }
+    init {
+        auth.addAuthStateListener { auth ->
+            if (auth.currentUser == null) {
+                return@addAuthStateListener
+            }
+            val refCurrUser = db.collection("users")
+                .document(auth.currentUser!!.uid)
+            refCurrUser.get().addOnSuccessListener {
+                it.toObject(FBUser::class.java)?.let { user ->
+                    user.toUser()?.let { it1 -> listener?.onUserLoaded(it1) }
+                }
+            }
+            getRestaurants()
+    }}
 
-//    init {
-//        auth.addAuthStateListener { auth ->
-//            if (auth.currentUser == null) {
-////                citiesListReg?.remove()
-//                return@addAuthStateListener
-//            }
-//            val refCurrUser = db.collection("users")
-//                .document(auth.currentUser!!.uid)
-//            refCurrUser.get().addOnSuccessListener {
-//                it.toObject(FBUser::class.java)?.let { user ->
-//                    listener?.onUserLoaded(user.toUser())
-//                }
-//            }
 
-//
-//        }
-//    }
      fun register(user: User) {
          if ( auth.currentUser == null) {
              throw RuntimeException("User not logged in!")
          }
         val uid = auth.currentUser!!.uid
-        db.collection("Users").document(uid + "").set(user.toFBUser());
+        db.collection("Users").document(uid + "").set(user.toFBUser())
      }
+
+
+    private fun getRestaurants() {
+        val restaurantsRef = db.collection("restaurants")
+        restaurantsRef.get().addOnSuccessListener { querySnapshot ->
+            val restaurants = querySnapshot.documents.mapNotNull { it.toObject(Restaurant::class.java) }
+            listener?.setRestaurants(restaurants)
+        }.addOnFailureListener { exception ->
+            Log.e("FirebaseDB", "Error fetching restaurants", exception)
+        }
+    }
+
+
+    fun saveRestaurant(restaurant: Restaurant) {
+        try {
+            db.collection("restaurants").document().set(restaurant)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
 }
--- app/src/main/java/ifpe/mobile/lactgoGo/src/ui/pages/ExplorePage.kt
+++ app/src/main/java/ifpe/mobile/lactgoGo/src/ui/pages/ExplorePage.kt
@@ -1,4 +1,4 @@
-package ifpe.mobile.lactgoGo.src.ui.theme.pages
+package ifpe.mobile.lactgoGo.src.ui.pages
 
 import android.widget.Toast
 import androidx.compose.foundation.clickable
@@ -11,16 +11,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -29,12 +34,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import ifpe.mobile.lactgoGo.src.MVM.MainViewModel
+import ifpe.mobile.lactgoGo.src.database.db.FirebaseDB
+import ifpe.mobile.lactgoGo.src.database.models.Restaurant
+import kotlinx.coroutines.launch
 
-data class Place(val name: String, var district: String, var workingHours: String)
 
 @Composable
 fun PlaceItem(
-    place: Place,
+    place: Restaurant,
     onPin: () -> Unit,
     onSelect: () -> Unit,
     modifier: Modifier = Modifier
@@ -57,12 +65,12 @@ fun PlaceItem(
             )
             Text(
                 modifier = Modifier,
-                text = place.district,
+                text = place.address,
                 fontSize = 16.sp
             )
             Text(
                 modifier = Modifier,
-                text = place.workingHours,
+                text = place.openingTime,
                 fontSize = 16.sp
             )
 
@@ -74,9 +82,10 @@ fun PlaceItem(
 }
 
 @Composable
-fun ExplorePageComp(modifier: Modifier = Modifier) {
+fun ExplorePageComp(modifier: Modifier = Modifier, database :FirebaseDB, viewModel: MainViewModel,) {
     var address by rememberSaveable { mutableStateOf("") }
     val context = LocalContext.current
+    val restaurantList by remember { mutableStateOf(viewModel.restaurants) }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -90,27 +99,25 @@ fun ExplorePageComp(modifier: Modifier = Modifier) {
                 .padding(16.dp, 16.dp, 16.dp, 8.dp)
                 .fillMaxWidth(),
         )
+
         Spacer(modifier = modifier.size(50.dp))
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(8.dp)
         ) {
-            items(30) { place ->
-                PlaceItem(
-                    place = Place(
-                        name = "Estabelecimento ",
-                        district = "Bairro",
-                        workingHours = "9am - 5pm"
-                    ),
-                    onSelect = {
-                        Toast.makeText(context, "Opening place", Toast.LENGTH_LONG).show()
-                    },
-                    onPin = {
-                        Toast.makeText(context, "Place pinned", Toast.LENGTH_LONG).show()
-                    },
-                )
+            items(restaurantList) {
+                restaurant -> PlaceItem(
+                place = restaurant,
+                onSelect = {
+                    Toast.makeText(context, "Opening ${restaurant.name}", Toast.LENGTH_LONG).show()
+                },
+                onPin = {
+                    Toast.makeText(context, "Place pinned", Toast.LENGTH_LONG).show()
+                },
+            )
             }
+
         }
     }
 }
