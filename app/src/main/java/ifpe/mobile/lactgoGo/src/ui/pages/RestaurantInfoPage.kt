d74ebe2abc78e4af337318d5fd47372c4fabec65
JorgeD <jorgelucasdantas@hotmail.com>
2024-09-15 21:49:23 -0300

    Setting up Restaurant infoPage with the menu

--- app/src/main/java/ifpe/mobile/lactgoGo/src/ui/pages/RestaurantInfoPage.kt
+++ app/src/main/java/ifpe/mobile/lactgoGo/src/ui/pages/RestaurantInfoPage.kt
@@ -1,59 +1,47 @@
 package ifpe.mobile.lactgoGo.src.ui.pages
 
 import android.content.Context
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ifpe.mobile.lactgoGo.src.MVM.MainViewModel
+import ifpe.mobile.lactgoGo.src.ui.composables.DishIndoCard
+import ifpe.mobile.lactgoGo.src.ui.composables.RestaurantInfoCard
 
 @Composable
 fun RestaurantInfoPage(
-    context: Context, viewModel : MainViewModel, modifier :Modifier,
+    context: Context,
+    modifier :Modifier,
+    viewModel : MainViewModel
+) {
+    val restaurant = viewModel.restaurant
+    val dishes = restaurant.menu
+
+
+    Column (
+        modifier.padding(top = 15.dp, bottom = 30.dp, start = 20.dp, end=20.dp)
     ) {
 
+        RestaurantInfoCard( modifier = modifier, restaurant = restaurant)
 
-
-    println("Banana ${viewModel.restaurant}")
-
-    Column(modifier.fillMaxHeight().fillMaxWidth()) {
-
-        Card(
+        HorizontalDivider(modifier.height(15.dp), color = Color.Transparent)
+        LazyColumn(
             modifier
-                .background(color = Color.White)
-                .widthIn(min = 350.dp)
-                .heightIn(min = 100.dp)
-                .align(Alignment.CenterHorizontally)
+                .fillMaxSize()
+                .padding(8.dp)
         ) {
-            Row {
-                Box(modifier.background(color = Color.Red).widthIn(min = 50.dp)) {}
-                Column {
-                    Text("ok")
-                    Text("ok")
-                    Text("ok")
-                }
-            }
-
-        }
-
-        Column (modifier.fillMaxHeight().background(color = Color.Yellow)){
-            Card {  }
+            items(dishes) { dish -> DishIndoCard(modifier = modifier, dish = dish) }
         }
     }
+
+
 
 }
